8075bbc09caa0a5fbb5868b3cd90eeb0a55afd0f
Ian Kesler <ianmkesler@gmail.com>
2019-12-13 12:49:33 -0800

    Basic html REST site

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,54 @@
+class KittensController < ApplicationController
+    def index
+        @kittens = Kitten.all
+    end
+
+    def show
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def new
+        @kitten = Kitten.new
+    end
+
+    def create
+        @kitten = Kitten.new(kitten_params)
+        if @kitten.save
+            flash[:success] = "Kitten created"
+            redirect_to @kitten
+        else
+            flash.now[:danger] = "Could not create"
+            render 'new'
+        end
+    end
+
+    def edit
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def update
+        @kitten = Kitten.find(params[:id])
+        if @kitten.update_attributes(kitten_params)
+            flash[:success] = "Updated"
+            redirect_to @kitten
+        else
+            flash.now[:danger] = "Could not update"
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @kitten = Kitten.find(params[:id])
+        if @kitten.destroy
+            flash[:success] = "Kitten deleted"
+        else
+            flash[:danger] = "Could not delete"
+        end
+        redirect_to kittens_path
+    end
+
+    private
+        def kitten_params
+            params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+        end
+end
